2ec20fa4d8aced40ac696439a02babace83c3a26
Gerrit <gerrit.gruben@gmail.com>
2015-01-16 01:41:16 +0100

    Started next chapter

--- src/main/scala/par/Par.scala
+++ src/main/scala/par/Par.scala
@@ -1,15 +1,29 @@
 package par
 
+import java.util.concurrent.{TimeUnit, Future, ExecutorService}
+
 /**
  * Created by gerrit on 03.01.15.
  */
-trait Par[A] {
-
-}
 
 object Par {
+  type Par[A] = ExecutorService => Future[A]
   // Obtains the result of a parallel computation.
-  def get[A](pa : Par[A]) : A = ???
+  def run[A](implicit es : ExecutorService)(pa : Par[A]) : Future[A] = pa(es)
+
+  def unit[A](a: A) : Par[A] = (es : ExecutorService) => UnitFuture(a)
   // Wraps a computation to be done into a parallel computation (e.g. by starting a thread to compute it).
-  def unit[A](a : => A) : Par[A] = ???
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def lazyUnit[A](a: => A) : Par[A] = fork(unit(a))
+
+  def map2[A,B,C](pa: => Par[A], pb : => Par[B])(f: (A,B) -> C) : Par[C] = ???
+  // =?= unit(f(get(pa), get(pb)))
+
+  def fork[A](pa : => Par[A]) : Par[A]
  }
